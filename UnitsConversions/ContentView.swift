d1702c4361b0d59ac854872ad501df449a8b8fa8
Sam McCullough <samanuel.mc@gmail.com>
2019-11-27 21:00:22 -0800

    Initial Commit

---
+++ UnitsConversions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitsConversions
+//
+//  Created by Sam on 2019-11-27.
+//  Copyright © 2019 Sailfish Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
