b5c4efe0aac0d69ba4026eaa6a5801d45a1e0407
Sam McCullough <samanuel.mc@gmail.com>
2019-11-27 23:03:01 -0800

    Build out UnitConversions ContentView

--- UnitsConversions/ContentView.swift
+++ UnitsConversions/ContentView.swift
@@ -12,10 +12,55 @@ struct ContentView: View {
     
     /* State Variables */
     @State private var inputNumber = ""
-    @State private var inputUnit = ""
-    @State private var outputUnit = ""
+    @State private var inputUnit = 0
+    @State private var outputUnit = 0
     
     let units = ["mL","L", "pt.", "qt.", "gal.", "fl.oz."]
+    
+    var outputNumber: String {
+        let amountToConvert = Double(inputNumber) ?? 0
+        let input = units[inputUnit]
+        let output = units[outputUnit]
+        
+        var inputVolume: Measurement<UnitVolume>
+        var outputVolume: Measurement<UnitVolume>
+        
+        switch input {
+        case "mL":
+            inputVolume = Measurement(value: amountToConvert, unit: UnitVolume.milliliters)
+        case "L":
+            inputVolume = Measurement(value: amountToConvert, unit: UnitVolume.liters)
+        case "pt.":
+            inputVolume = Measurement(value: amountToConvert, unit: UnitVolume.pints)
+        case "qt.":
+            inputVolume = Measurement(value: amountToConvert, unit: UnitVolume.quarts)
+        case "gal.":
+            inputVolume = Measurement(value: amountToConvert, unit: UnitVolume.gallons)
+        case "fl.oz.":
+            inputVolume = Measurement(value: amountToConvert, unit: UnitVolume.fluidOunces)
+        default:
+            inputVolume = Measurement(value: amountToConvert, unit: UnitVolume.milliliters)
+        }
+        
+        switch output {
+        case "mL":
+            outputVolume = inputVolume.converted(to: UnitVolume.milliliters)
+        case "L":
+            outputVolume = inputVolume.converted(to: UnitVolume.liters)
+        case "pt.":
+            outputVolume = inputVolume.converted(to: UnitVolume.pints)
+        case "qt.":
+            outputVolume = inputVolume.converted(to: UnitVolume.quarts)
+        case "gal.":
+            outputVolume = inputVolume.converted(to: UnitVolume.gallons)
+        case "fl.oz.":
+            outputVolume = inputVolume.converted(to: UnitVolume.fluidOunces)
+        default:
+            outputVolume = inputVolume.converted(to: UnitVolume.milliliters)
+        }
+        
+        return MeasurementFormatter().string(from: outputVolume)
+    }
     
     var body: some View {
         NavigationView {
@@ -25,8 +70,7 @@ struct ContentView: View {
                 
                 TextField("Amount", text: $inputNumber)
                     .keyboardType(.decimalPad)
-                    .padding(.all)
-                    .background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
                 
                 Text("Select the input unit type")
                     .font(.headline)
@@ -48,14 +92,19 @@ struct ContentView: View {
                 }
                 .pickerStyle(SegmentedPickerStyle())
                 
+                HStack {
+                    Text("The converted value is")
+                    Text("\(outputNumber)")
+                        .font(.headline)
+                }
+                .padding(.top, 15)
+                
                 Spacer()
             }
             .padding(.horizontal, 15)
             .navigationBarTitle("Volume Converter")
         }
     }
-    
-    func convertUnit(
 }
 
 struct ContentView_Previews: PreviewProvider {
